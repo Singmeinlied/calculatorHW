55915cbe172c30932c7669c0e00be58ed628ca50
Singmeinlied <98156702+Singmeinlied@users.noreply.github.com>
2022-06-11 18:41:47 +0600

    added new buttons

---
+++ Calculator/calculatorVC.swift
@@ -0,0 +1,18 @@
+//
+//  calculatorVC.swift
+//  Calculator
+//
+//  Created by Abai Kush on 10/6/22.
+//
+
+import UIKit
+
+class calculatorVC: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
